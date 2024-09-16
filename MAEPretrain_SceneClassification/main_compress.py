fix final embedding size calculation after training

MAEPretrain_SceneClassification/main_compress.py
@@ -9,8 +9,6 @@
 # BEiT: https://github.com/microsoft/unilm/tree/master/beit
 # --------------------------------------------------------
 
-# Copyright contributors to the neural-embedding-compression project
-
 import argparse
 import datetime
 import json
@@ -48,7 +46,13 @@ def get_args_parser():
         "--batch_size",
         default=64,
         type=int,
-        help="Batch size per GPU (effective batch size is batch_size * accum_iter * # gpus",
+        help="Batch size per GPU (effective batch size is batch_size * accum_iter * # gpus)",
+    )
+    parser.add_argument(
+        "--eval_batch_size",
+        default=64,
+        type=int,
+        help="Batch size per GPU for eval.(effective batch size is batch_size * accum_iter * # gpus). For the final eval, only 1 gpu is used, so the effective batch size is the same as this)",
     )
     parser.add_argument("--epochs", default=400, type=int)
     parser.add_argument(
@@ -311,7 +315,7 @@ def main(args):
     data_loader_eval = torch.utils.data.DataLoader(
         dataset_eval,
         sampler=sampler_eval,
-        batch_size=64,
+        batch_size=args.eval_batch_size,
         num_workers=args.num_workers,
         pin_memory=args.pin_mem,
         drop_last=False,
@@ -435,8 +439,17 @@ def main(args):
     model_without_ddp.update()
     # do this without ddp
 
+    dataloader = torch.utils.data.DataLoader(
+        dataset_eval,
+        batch_size=args.eval_batch_size,
+        num_workers=args.num_workers,
+        pin_memory=args.pin_mem,
+        drop_last=False,
+        shuffle=False,
+    )
+
     if not args.distributed or global_rank == 0:
-        embedding_sizes = findEmbeddingSize(model_without_ddp, data_loader_eval)
+        embedding_sizes = findEmbeddingSize(model_without_ddp, dataloader)
         av_size = (embedding_sizes.sum() / args.eval_size) * 8
         print(f"Average bits per image: {av_size}")
         log_writer.add_scalar("Average bits", av_size)

MAEPretrain_SceneClassification/util/compression.py
@@ -1,4 +1,3 @@
-# Copyright contributors to the neural-embedding-compression project
 
 from typing import Any, Dict, Mapping, cast
 
@@ -92,13 +91,4 @@ def findEmbeddingSize(model, dataloader, extract_data_ld=None, device=0):
             bytes_used.append(total_bytes_batch)
 
     bytes_used = torch.tensor(bytes_used)
-    if not is_dist_avail_and_initialized():
-        return bytes_used
-    bytes_used = bytes_used.to(device=device)
-    dist.barrier()
-    all_bytes_used = [
-        torch.zeros_like(bytes_used) for _ in range(dist.get_world_size())
-    ]  # world_size is the total number of gpu processes you are running. 4 in your case.
-    print(dist.get_world_size())
-    all_bytes_used = torch.cat(dist.all_gather(all_bytes_used, bytes_used))
-    return all_bytes_used
+    return bytes_used